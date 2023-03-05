67cdfa0062c506580e4f82884f5730bae27e4dd4
jaime7981 <jmfernandez1@miaundes.cl>
2023-03-05 14:30:35 -0300

    docker database added as well as a gitignore

---
+++ dockerDatabase/sqlQueries/basic_setup.sql
@@ -0,0 +1,48 @@
+CREATE TABLE player (
+    id SERIAL PRIMARY KEY,
+    player_name VARCHAR(50),
+    passwrd TEXT
+);
+
+CREATE OR REPLACE FUNCTION check_username(username VARCHAR(50))
+RETURNS VOID AS $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM player WHERE player_name = username) THEN
+        RAISE EXCEPTION 'Username already exists: %', username;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION prevent_duplicate_usernames()
+RETURNS TRIGGER AS $$
+BEGIN
+    PERFORM check_username(NEW.player_name);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER unique_username_trigger
+BEFORE INSERT ON player
+FOR EACH ROW
+EXECUTE FUNCTION prevent_duplicate_usernames();
+
+
+CREATE TABLE faction (
+    id SERIAL PRIMARY KEY,
+    faction_name VARCHAR(50)
+);
+
+CREATE TABLE faction_members (
+    id SERIAL PRIMARY KEY,
+    player_id INTEGER REFERENCES player(id),
+    faction_id INTEGER REFERENCES faction(id)
+);
+
+CREATE TABLE faction_relationship (
+    id SERIAL PRIMARY KEY,
+    faction_one_id INTEGER REFERENCES faction(id),
+    faction_two_id INTEGER REFERENCES faction(id),
+    relationship VARCHAR(20)
+);
+
+

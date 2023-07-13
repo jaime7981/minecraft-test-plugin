0d52b8064557e23fa2574e01775a942d9d977762
jaime7981 <jmfernandez1@miaundes.cl>
2023-07-13 17:54:28 -0400

    updated item cost path

--- compile_plugin.sh
+++ compile_plugin.sh
@@ -1,3 +1,3 @@
 mvn clean compile assembly:single -f /mine_plugin/pom.xml
 cp /mine_plugin/target/testplugin-1.0-SNAPSHOT-jar-with-dependencies.jar /mine_server/plugins
-cp /mine_plugin/target/classes/xyz/developmentcl/commands/ItemsCost.json /mine_server/plugins
+cp /mine_plugin/src/main/java/xyz/developmentcl/commands/ItemsCost.json /mine_server/plugins

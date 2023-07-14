944acd5a2a2e44c63e076ded8d0d1073a1984945
jaime7981 <jmfernandez1@miaundes.cl>
2023-07-13 20:19:41 -0400

    tests and fixed function

--- src/main/java/xyz/developmentcl/game_functions/GameFunctions.java
+++ src/main/java/xyz/developmentcl/game_functions/GameFunctions.java
@@ -9,21 +9,15 @@ import org.bukkit.entity.Player;
 
 public class GameFunctions {
     public static boolean isPointInRectangularPrism(double x1, double y1, double z1, double x2, double y2, double z2, double px, double py, double pz) {
-        // Calculate the vectors from the top-left-front corner to the other corners
-        double[] v1 = {x2 - x1, y2 - y1, z2 - z1};
-        double[] v2 = {px - x1, py - y1, pz - z1};
+        double minX = Math.min(x1, x2);
+        double maxX = Math.max(x1, x2);
+        double minY = Math.min(y1, y2);
+        double maxY = Math.max(y1, y2);
+        double minZ = Math.min(z1, z2);
+        double maxZ = Math.max(z1, z2);
 
-        // Calculate the scalar triple product
-        double scalarTripleProduct = v1[0] * (v2[1] * v1[2] - v2[2] * v1[1])
-                + v1[1] * (v2[2] * v1[0] - v2[0] * v1[2])
-                + v1[2] * (v2[0] * v1[1] - v2[1] * v1[0]);
-
-        // Check if the scalar triple product is zero (point lies on one of the prism faces)
-        if (scalarTripleProduct == 0) {
-            // Check if the prism has a non-zero volume
-            if (v1[0] != 0 || v1[1] != 0 || v1[2] != 0) {
-                return true;
-            }
+        if (px >= minX && px <= maxX && py >= minY && py <= maxY && pz >= minZ && pz <= maxZ) {
+            return true;
         }
         return false;
     }
@@ -49,11 +43,14 @@ public class GameFunctions {
             // Get the faction's safe zone corners
             factionSafeZoneCorners = faction.getSafeCoordinates();
 
-            // Check if the player is in the faction's safe zone
-            if (isPointInRectangularPrismCoordinates(factionSafeZoneCorners, itemLocation)) {
-                // check if player is not in faction
-                if (!faction.isPlayerOnFaction(player.getName())) {
-                    return false;
+            // check if faction has safe zone
+            if (faction.getSafeCoordinates() != null && !faction.getSafeCoordinates().isEmpty()) {
+                // Check if the player is in the faction's safe zone
+                if (isPointInRectangularPrismCoordinates(factionSafeZoneCorners, itemLocation)) {
+                    // check if player is not in faction
+                    if (!faction.isPlayerOnFaction(player.getName())) {
+                        return false;
+                    }
                 }
             }
         }
--- src/test/java/xyz/developmentcl/AppTest.java
+++ src/test/java/xyz/developmentcl/AppTest.java
@@ -1,8 +1,14 @@
 package xyz.developmentcl;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.bukkit.Location;
 import org.junit.Test;
+
+import xyz.developmentcl.game_functions.GameFunctions;
 
 /**
  * Unit test for simple App.
@@ -16,5 +22,47 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void isPointOnSpace()
+    {
+        List<List<Integer>> coordinates = List.of(List.of(43, 53, -3), List.of(63, 73, 17));
+        Location location = new Location(null, 53, 63, 7);
+
+        if (GameFunctions.isPointInRectangularPrismCoordinates(coordinates, location)) {
+            assertTrue( true );
+        }
+        else {
+            assertTrue( false );
+        }
+    }
+
+    @Test
+    public void pointIsNotOnSpace1()
+    {
+        List<List<Integer>> coordinates = List.of(List.of(43, 53, -3), List.of(63, 73, 17));
+        Location location = new Location(null, 33, 63, 10);
+
+        if (!GameFunctions.isPointInRectangularPrismCoordinates(coordinates, location)) {
+            assertTrue( true );
+        }
+        else {
+            assertTrue( false );
+        }
+    }
+
+    @Test
+    public void pointIsNotOnSpace2()
+    {
+        List<List<Integer>> coordinates = List.of(List.of(43, 53, -3), List.of(63, 73, 17));
+        Location location = new Location(null, 50, 63, 19);
+
+        if (!GameFunctions.isPointInRectangularPrismCoordinates(coordinates, location)) {
+            assertTrue( true );
+        }
+        else {
+            assertTrue( false );
+        }
     }
 }
